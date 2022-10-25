9fa7a56feb7d33b4e5ffd945a27ad0024fc6bbe7
Rita1202 <Rita.sokolchenko@gmail.com>
2022-10-25 21:07:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import Axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
